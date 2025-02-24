db09b78b7ae1f80f8a5cd04498b79ebfbf1ced1b
Yumeth <yumethsumathipala@gmail.com>
2025-02-24 23:55:33 +0530

    feat: Integrate Firebase into Flutter project
    This commit integrates Firebase into the Flutter project, enabling Firebase services for the Android app. The changes include:
    
    -   **Firebase Configuration Files:**
        -   Added `firebase.json` to configure Firebase project settings.
        -   Added `lib/firebase_options.dart` to handle platform-specific Firebase initialization.
        -   Added `android/app/google-services.json` with project-specific configurations for the Android app.
    -   **Gradle Configuration:**
        -   Modified `android/app/build.gradle.kts` to apply the Google Services plugin (`com.google.gms.google-services`).
        -   Updated `android/settings.gradle.kts` to include the Google Services plugin with version.
    -   **Firebase Core Dependency:**
        -   Added `firebase_core` dependency to `pubspec.yaml` and `pubspec.lock` to enable Firebase core functionality.
    - **Firebase Initialization:**
        - Modified `main.dart` to initialize Firebase using `Firebase.initializeApp` and `DefaultFirebaseOptions.currentPlatform` method.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:ai_guardian/firebase_options.dart';
 import 'package:ai_guardian/screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
